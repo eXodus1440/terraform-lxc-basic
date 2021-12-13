3a0e35b271f0d3acca52a69308590d98b2fecb7a
Steve Bauer <eXodus1440@users.noreply.github.com>
2021-12-13 08:08:47 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,25 +1,15 @@
-// Provider config
-variable "pm_api_url" {
-  default = "https://<PROXMOX IP>:8006/api2/json"
-}
-
-variable "pm_user" {
-  default = "root@pam"
-}
-
-variable "pm_password" {
-  default = "root@pam"
-}
+// Provider config - create a tfvars file and populate it with your connection details
+variable "pm_api_url" { default = "https://<PROXMOX IP>:8006/api2/json" }
+variable "pm_user" { default = "root@pam" }
+variable "pm_password" { default = "root@pam" sensitive = true }
 
 // Resource config
-variable "target_node" {
-  default = "pve1"
-}
+variable "target_node" { default = "pve1" }
 variable "ostemplate" { default = "local:vztmpl/ubuntu-21.04-standard_21.04-1_amd64.tar.gz" }
 variable "ostype" { default = "ubuntu" }
 variable "onboot" { default = true }
 variable "start" { default = true }
-variable "password" { default = "BasicLXCContainer" }
+variable "password" { default = "BasicLXCContainer" sensitive = true }
 
 // Resource root FS
 variable "storage" { default = "local-lvm" }
