1ca0350687d96b09e5f6b77d5668f590480c347a
Steve Bauer <steven@bauer.com.au>
2021-12-13 08:11:35 +0000

    updated variables to fix json syntax

--- variables.tf
+++ variables.tf
@@ -1,7 +1,10 @@
 // Provider config - create a tfvars file and populate it with your connection details
 variable "pm_api_url" { default = "https://<PROXMOX IP>:8006/api2/json" }
 variable "pm_user" { default = "root@pam" }
-variable "pm_password" { default = "root@pam" sensitive = true }
+variable "pm_password" { 
+  default = "root@pam" 
+  sensitive = true 
+}
 
 // Resource config
 variable "target_node" { default = "pve1" }
@@ -9,7 +12,10 @@ variable "ostemplate" { default = "local:vztmpl/ubuntu-21.04-standard_21.04-1_am
 variable "ostype" { default = "ubuntu" }
 variable "onboot" { default = true }
 variable "start" { default = true }
-variable "password" { default = "BasicLXCContainer" sensitive = true }
+variable "password" { 
+  default = "BasicLXCContainer" 
+  sensitive = true 
+}
 
 // Resource root FS
 variable "storage" { default = "local-lvm" }
